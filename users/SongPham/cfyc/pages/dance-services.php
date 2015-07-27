590883533e14fd8ed8e318bc2b476f1649bca030
songpham <song20002005@yahoo.com>
2015-07-27 17:19:25 +0700

    separate dance-services page

---
+++ users/SongPham/cfyc/pages/dance-services.php
@@ -0,0 +1,11 @@
+<div class="container-fluid page dance-services">
+<?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
+<div class="container-page">
+<?php include USER_BASE_PATH . '/pages/dance-services/top.php'; ?>
+<?php include USER_BASE_PATH . '/pages/dance-services/bottom.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/dance-services-top.js"></script>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/dance-services-bottom.js"></script>
