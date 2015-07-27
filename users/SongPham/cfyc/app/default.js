e97832524e7c214e03a46e4e28de44d91cfe83a0
cfyc_pages <GlenCoffey@cfyc.com.vn>
2015-07-27 18:23:46 +0700

    Desktop and Mobile menus. - Need to finish mobile submenus and style

---
+++ users/SongPham/cfyc/app/default.js
@@ -0,0 +1,23 @@
+        // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    
+    /* adds/removes hidden class for main menu submenus */
+    
+    $('#desktop-nav a').click(function (e) {
+			    if($(this).parent('li').hasClass('active')){
+			        $( $(this).attr('href') ).hide();
+			    }
+			    else {
+			        e.preventDefault();
+			        $(this).tab('show');
+			    }
+			});
