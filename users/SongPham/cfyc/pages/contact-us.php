6bdcf7fef0072d29d01ece1e5a7761937989da48
boducoscar <amthucgiaitri@gmail.com>
2015-07-28 16:39:39 +0700

    check in code

---
+++ users/SongPham/cfyc/pages/contact-us.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * Contact us page
+ */
+?>
+<div class="container-fluid page contact-us">
+<?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
+<div class="container-page">
+<h2>contact us now</h2>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/contact-us.js"></script>
--- users/SongPham/cfyc/pages/home.php
+++ users/SongPham/cfyc/pages/home.php
@@ -1,20 +1,190 @@
 <div class="container-fluid page home-page">
 <?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
-<div class="row gallery">
-    <img src="<?php echo USER_BASE_URL ?>/img/gallery.jpg" alt="gallery" />
-    <a class="down-button" href="javascript:void(0)">&nbsp;</a>
-</div>
 <div class="container-page">
+    <!-- Top Banner -->
+    <div class="row gallery">       
+        <div id="movetopbanner" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#movetopbanner" data-slide-to="0" class="active"></li>
+            <li data-target="#movetopbanner" data-slide-to="1"></li>
+            <li data-target="#movetopbanner" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img class="first-slide" src="img/home/california-wow-banner-1.jpg" alt="First slide">          
+            </div>
+            <div class="item">
+              <img class="first-slide" src="img/home/california-wow-banner-1.jpg" alt="Second slide">          
+            </div>
+            <div class="item">
+               <img class="first-slide" src="img/home/california-wow-banner-1.jpg" alt="Third slide">          
+            </div>
+          </div>
+          <a class="left carousel-control" href="#movetopbanner" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#movetopbanner" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+    </div><!-- /.Top Banner -->
     <div class="row top-content">
         <div class="holder-top-content">
             <div class="col-md-12 text-center">
-                <h1 class="subject">Life Enhancement Services</h1>
-                <img class="hr" src="<?php echo USER_BASE_URL ?>/img/hr.jpg" alt="hr" />
-                <div class="text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nobis, eligendi vel! Dolorum dolor quis itaque magni, tempore quae fugit possimus perferendis dicta beatae quos necessitatibus maiores, non veniam aliquid.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nobis, eligendi vel! Dolorum dolor quis itaque magni, tempore quae fugit possimus perferendis dicta beatae quos necessitatibus maiores, non veniam aliquid.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nobis, eligendi vel! Dolorum dolor quis itaque magni, tempore quae fugit possimus perferendis dicta beatae quos necessitatibus maiores, non veniam aliquid.</p>
+                <h2><span>OUR SERVICES</span></h2>
+                <p>Science doesn't lie and neither do we. Our clinically proven approach is fueled by reality. The fantasy is where you come in.</p>
+                <div class="row ourservice">
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-1.jpg" alt="Personal Trainner" title="Personal Trainner">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Personal Trainner</h2>
+                       </div>
+                    </div>
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-2.jpg" alt="Yoga" title="Yoga">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Yoga</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-3.jpg" alt="Group Exercise" title="Group Exercise">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Group Exercise</h2>
+                        </div>
+                    </div>
                 </div>
+                <div class="row ourservice">
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-4.jpg" alt="Kid's Program" title="Kid's Program">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Kid's Program</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-5.jpg" alt="Spa esthic" title="Spa esthic">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Spa esthic</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4 service_item">
+                        <div class="service_img">
+                            <img src="img/home/california-wow-our-services-6.jpg" alt="Kickfit" title="Kickfit">
+                        </div>
+                        <div class="overlay">
+                            <h2 class="service_title">Kickfit</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <div class="row">
+        <div id="box" class="col-md-4 service_item">
+            <div class="image">
+                <img src="http://s18.postimg.org/il7hbk7i1/image.png" />
+            </div>
+            <div class="overlay">
+                <a class="mylink">link</a>
+            </div>
+        </div>
+    </div>
+    <style type="text/css">
+    .service_item {
+    text-align: center;
+    margin: auto;
+    width: 250px;
+    height: 250px;
+    overflow: hidden;
+    position: relative;
+}
+
+.image img {
+    display:block;
+    width: 250px;
+    height: 250px;
+
+    transform:scale(1);
+    -ms-transform:scale(1);
+    -moz-transform:scale(1);
+    -webkit-transform:scale(1);
+    -o-transform:scale(1);
+    -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+    transition: all 1s ease;
+}
+
+.service_item:hover .image img {    
+    cursor: pointer;
+    transform:scale(1.5);
+    -ms-transform:scale(1.5);
+    -moz-transform:scale(1.5);
+    -webkit-transform:scale(1.5);
+    -o-transform:scale(1.5);
+}
+
+.overlay{
+    position: absolute;    
+    width: 100%;
+    height: 100%;
+    top: 0;
+    -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+    transition: all 1s ease;
+}
+
+.overlay:hover{
+    background: rgba(255, 255, 255, 0.5);    
+}
+
+.overlay h2{
+    position: absolute;
+    /*top: -100%;*/   
+     -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+    transition: all 1s ease;
+    bottom: 0
+}
+
+.overlay:hover h2{    top: 50%;
+}
+</style>
+            <div class="col-md-12 text-center">
+                <h2><span>FIND YOUR CLUB</span></h2>
+                <p>There’s training, and there’s Cali training. Our science-fueled approach plus your commitment means goals get met. It’s like school (except you’re hotter now).</p>
+            </div>
+            <div class="col-md-12 text-center">
+                <h2><span>FIND YOUR TRAINER</span></h2>
+                <p>There’s training, and there’s Cali training. Our science-fueled approach plus your commitment means goals get met. It’s like school (except you’re hotter now).</p>
+            </div>
+            <div class="col-md-12 text-center">
+                <h2><span>SUCCESS STORIES</span></h2>
+                <p>It’s a 24/7 hangout for high-performance living. Smart, evocative, ahead of the curve. Get inspired, get informed, get it here.</p>
+            </div>
+            <div class="col-md-12 text-center">
+                <h2><span>CELEBRITIES MEMBERS</span></h2>
+                <p>There’s training, and there’s Cali training. Our science-fueled approach plus your commitment means goals get met. It’s like school (except you’re hotter now).</p>
+            </div>
+            <div class="col-md-12 text-center">
+                <h2><span>FEATURE STORIES</span></h2>
+                <p>There’s training, and there’s Cali training. Our science-fueled approach plus your commitment means goals get met. It’s like school (except you’re hotter now).</p>
+            </div>
+            <div class="col-md-12 text-center">
+                <h2><span>SPECIAL OFFERS</span></h2>
+                <p>There’s training, and there’s Cali training. Our science-fueled approach plus your commitment means goals get met. It’s like school (except you’re hotter now).</p>
             </div>
         </div>
     </div>
