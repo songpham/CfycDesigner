b5c68a48295a10d3d6cce22e4e87d9a7ebdae957
aaa <teo@yahoo.com>
2015-07-28 17:50:44 +0700

    update celebrity-trainning.php

---
+++ users/SongPham/cfyc/pages/celebrity-trainning.php
@@ -0,0 +1,211 @@
+<div class="container-fluid page celebrity-trainning-page">
+
+
+    <!-- GALLERY -->
+    <div class="row gallery">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="<?php echo USER_BASE_URL . "/img/celebrity-training/gallery-1.jpg" ?>" alt="">
+                    <div class="carousel-caption">
+                        Your image is your brand let us help you build it
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?php echo USER_BASE_URL . "/img/celebrity-training/gallery-2.jpg" ?>" alt="">
+                    <div class="carousel-caption">
+                        Your image is your brand let us help you build it
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END GALLERY -->
+
+
+    <!-- CONTAINER 1 -->
+    <div class="row container-page">
+
+
+        <!-- ABOUT CELEBRITY TRANINING -->
+        <div class="row block about-block">
+            <div class='block-title'>
+                <span>about cerlebrity</span><br><span>training</span>
+            </div>
+            <div class="col-md-6 text">
+                <p>The friend who listens to your story and cares about your dreams and the expert who makes the plan and guides you to reach your goals.</p>
+                <p>Personal training at California Fitness & Yoga offers exclusive one-on-one programs specifically designed for your body's needs and your individual fitness goals. Our program is about integration, not sacrifice. You don't need to spend hours in the gym or starve yourself in order to achieve the body of your dreams.</p>
+                <p>Our patented California Health System is designed to integrate into your lifestyle using proven methods developed over dozens of year to help you get results in the fastest, safest way possible. Each of our trainers is highly qualified under international standards of certification following NASM Sports & Fitness certification curricula from the USA.</p>
+            </div>
+            <div class="col-md-5 col-md-offset-1 col-xs-12 video">
+                <img src='<?php echo USER_BASE_URL . "/img/celebrity-training/video.png" ?>' width='100%'>
+            </div>
+        </div>
+        <!-- END ABOUT CELEBRITY TRAINING -->
+
+
+        <!-- HOW DOES PT WORK ? -->
+        <div class="row block how-block">
+            <div class="block-title">
+                <span>How does PT work?</span>
+            </div>
+            <p class="text-center">We make it easy for you, very easy. Personal training is designed to integrate into your life without having to make sacrifices. </span>
+            <div class='col-md-6 left-img'><img src="<?php echo USER_BASE_URL . "/img/celebrity-training/how-1.jpg" ?>" width='100%'></div>
+            <div class='col-md-6  right-img'><img src="<?php echo USER_BASE_URL . "/img/celebrity-training/how-2.jpg" ?>" width='100%'></div>
+        </div>
+        <!-- END HOW DOES PT WORK ? -->
+
+
+    </div>
+    <!-- END CONTAINER 1 -->
+
+
+    <!-- BANNER -->
+    <div class="row banner">
+        <div class="text-content">
+            <h3>Meet our professional team of instructors specializing in celebrity training</h3>
+            <p>You have the potential, we can help you reach it.</p>
+            <ul class="nav navbar-nav navbar-center">
+                <li class="active"><a href="#">hcmc</a></li>
+                <li><a href="#">ha noi</a></li>
+                <li><a href="#">da nang</a></li>
+                <li><a href="#">binh duong</a></li>
+                <li><a href="#">bien hoa</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- END BANNER -->
+
+
+    <!-- CONTAINER 2 -->
+    <div class='row container-page'>
+
+
+        <!-- TRAINNER LIST -->
+        <div class="row block trainer-list-block">
+            <div class="col-md-6 trainer-avatar"></div>
+            <div class="col-md-6 intro-text">
+                <div class="trainer-name">linh ta</div>
+                <div class="trainer-job">master trainer</div>
+                <div class="trainer-info">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim eleifend porta. Vestibulum vel mi porta, sagittis odio vitae, faucibus odio. Nullam nulla est, efficitur non nisi ac, lacinia gravida diam. Aenean faucibus sapien sit amet gravida consectetur. Cras accumsan auctor porttitor. Vestibulum ut metus ullamcorper nunc cursus fermentum ac non nisi. Nulla dapibus eleifend pulvinar. Aenean sed malesuada tellus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim eleifend porta. Vestibulum vel mi porta, sagittis odio vitae, faucibus odio. Nullam nulla est, efficitur non nisi ac, lacinia gravida diam. Aenean faucibus sapien sit amet gravida consectetur. Cras accumsan auctor porttitor. Vestibulum ut metus ullamcorper nunc cursus fermentum ac non nisi. Nulla dapibus eleifend pulvinar. Aenean sed malesuada tellus.</p>
+                </div>
+            </div>
+        </div>
+        <!-- END TRAINER LIST -->
+
+
+        <!-- SUCCESS STORIES -->
+        <div class="row block success-story-block">
+            <div class="block-title">
+                <span>success stories</span>
+            </div>
+            <div class="col-md-12 box">
+                <div class="col-md-2 image"><img src="<?php echo USER_BASE_URL ?>/img/celebrity-training/story-1.png" alt=""></div>
+                <div class="col-md-10 text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim eleifend porta. Vestibulum vel mi porta, sagittis odio vitae, faucibus odio. Nullam nulla est, efficitur non nisi ac, lacinia gravida diam. Aenean faucibus sapien sit amet gravida consectetur. Cras accumsan auctor porttitor. Vestibulum ut metus ullamcorper nunc cursus fermentum ac non nisi. Nulla dapibus eleifend pulvinar. Aenean sed malesuada tellus.</div>
+            </div>
+            <div class="col-md-12 box">
+                <div class="col-md-2 image"><img src="<?php echo USER_BASE_URL ?>/img/celebrity-training/story-2.png" alt=""></div>
+                <div class="col-md-10 text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim eleifend porta. Vestibulum vel mi porta, sagittis odio vitae, faucibus odio. Nullam nulla est, efficitur non nisi ac, lacinia gravida diam. Aenean faucibus sapien sit amet gravida consectetur. Cras accumsan auctor porttitor. Vestibulum ut metus ullamcorper nunc cursus fermentum ac non nisi. Nulla dapibus eleifend pulvinar. Aenean sed malesuada tellus.</div>
+            </div>
+            <div class="col-md-12 see-more-btn">
+                <a href="#">see more</a>
+            </div>
+        </div>
+        <!-- END SUCCESS STORIES -->
+
+    </div>
+    <!-- END CONTAINER 2 -->
+
+
+    <div class="row" sytle='height: 1000px;'>	
+            <div id="cbp-contentslider" class="cbp-contentslider">
+                <ul>
+                    <li id="slide1">
+                        <h3 class="icon-wolf">Wolf</h3>
+                        <div>
+                            <div class="cbp-content">
+                                <p>Veggies sunt bona vobis, proinde vos postulo esse magis desert raisin bush tomato soko salsify garbanzo horseradish cabbage burdock.</p>
+                                <p>Daikon artichoke gumbo azuki bean bok choy avocado winter purslane gram earthnut pea broccoli salsify squash plantain wattle seed wakame broccoli rabe bamboo shoot. Zucchini lotus root azuki bean epazote dulse pumpkin rutabaga spinach. Endive mung bean gumbo maize plantain rock melon.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li id="slide2">
+                        <h3 class="icon-rabbit">Rabbit</h3>
+                        <div>
+                            <div class="cbp-content">
+                                <p>Chicory collard greens tatsoi cress bamboo shoot broccoli rabe lotus root earthnut pea arugula okra welsh onion leek shallot courgette. Chard garlic fava bean pea sprouts gram spinach plantain tomatillo. Chicory garlic black-eyed pea gourd chickpea cucumber lotus root.</p>
+                                <p>Daikon artichoke gumbo azuki bean bok choy avocado winter purslane gram earthnut pea broccoli salsify squash plantain wattle seed wakame broccoli rabe bamboo shoot. Zucchini lotus root azuki bean epazote dulse pumpkin rutabaga spinach. Endive mung bean gumbo maize plantain rock melon.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li id="slide3">
+                        <h3 class="icon-aligator">Aligator</h3>
+                        <div>
+                            <div class="cbp-content">
+                                <p>Tomato shallot broccoli rabe aubergine melon fava bean soybean sorrel. Tomatillo plantain wattle seed pea ricebean sorrel fennel welsh onion kakadu plum celery. Arugula radicchio garlic welsh onion kale celtuce tomato fava bean beet greens soybean burdock groundnut lentil broccoli rabe.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li id="slide4">
+                        <h3 class="icon-turtle">Turtle</h3>
+                        <div>
+                            <div class="cbp-content">
+                                <p>Yarrow bush tomato beetroot taro water chestnut komatsuna azuki bean. Parsnip artichoke water chestnut caulie carrot grape summer purslane brussels sprout fava bean tomatillo pea groundnut nori earthnut pea tigernut. Tigernut brussels sprout wakame tomatillo radicchio carrot maize swiss chard seakale melon grape broccoli pea sprouts kakadu plum plantain cauliflower. Gram salsify radicchio daikon courgette rock melon bunya nuts turnip greens. Cabbage black-eyed pea spinach asparagus plantain cress soybean spring onion salad artichoke pea garlic silver beet grape water spinach zucchini wakame summer purslane.</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li id="slide5">
+                        <h3 class="icon-platypus">Platypus</h3>
+                        <div>
+                            <div class="cbp-content">
+                                <p>Potato watercress burdock spinach quandong jícama chard pea sprouts garlic celery turnip daikon kale lotus root sorrel coriander rock melon cabbage. Epazote salsify lentil rock melon eggplant cauliflower zucchini caulie catsear broccoli rabe pumpkin lettuce okra green bean squash seakale onion water chestnut. Sweet pepper peanut corn soko celery winter purslane.</p>
+                                <p>Daikon artichoke gumbo azuki bean bok choy avocado winter purslane gram earthnut pea broccoli salsify squash plantain wattle seed wakame broccoli rabe bamboo shoot. Zucchini lotus root azuki bean epazote dulse pumpkin rutabaga spinach. Endive mung bean gumbo maize plantain rock melon.</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <nav>
+                    <a href="#slide1" class="icon-wolf"><span>Wolf</span></a>
+                    <a href="#slide2" class="icon-rabbit"><span>Rabbit</span></a>
+                    <a href="#slide3" class="icon-aligator"><span>Aligator</span></a>
+                    <a href="#slide4" class="icon-turtle"><span>Turtle</span></a>
+                    <a href="#slide5" class="icon-platypus"><span>Platypus</span></a>
+                </nav>
+            </div>
+    </div>
+
+    <?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/celebrity-trainning.js"></script>
+<script>
+    $(function() {
+        /*
+         - how to call the plugin:
+         $( selector ).cbpContentSlider( [options] );
+         - options:
+         {
+         // default transition speed (ms)
+         speed : 500,
+         // default transition easing
+         easing : 'ease-in-out',
+         // current item's index
+         current : 0
+         }
+         - destroy:
+         $( selector ).cbpContentSlider( 'destroy' );
+         */
+
+        $('#cbp-contentslider').cbpContentSlider();
+    });
+</script>
