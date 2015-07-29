c9d6887a64f8e79aca141ca3c42f06d6b3b896cd
Thai Anh <root>
2015-07-29 16:03:02 +0700

    add code

---
+++ users/SongPham/cfyc/pages/club.php
@@ -0,0 +1,30 @@
+<div class="container-fluid page club-page">
+<?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
+<div class="container-page">
+    <section class="club-introduction-widget">
+        <div class="row club-introduction-detail">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                <p class="club-introduction-content">Discover our wide variety of fitness, health and lifestyle clubs available throughout Ho Chi Minh City. Since 2007 when our first five star location opened up in Houng Voung Plaza, District 5, we have grown rapidly.</p>
+            </div>
+        </div>
+    </section>
+    <section class="club-detail-widget">
+        <div class="row selection-center">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h3>Choose your club</h3>
+                <div class="center-name">
+                    <span class="left-hz"></span>
+                    <span class="location">tan binh district</span>
+                    <span class="right-hz"></span>
+                </div>
+            </div>
+        </div>
+        <div class="row selection-center-detail">
+
+        </div>
+    </section>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/club.js"></script>
