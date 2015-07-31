9360ebfa18a8f0ff53ec7bb8068ab90577a628db
boducoscar <amthucgiaitri@gmail.com>
2015-07-31 16:35:01 +0700

    update home celebrity -> hover effect border & text have red color

--- users/SongPham/cfyc/pages/home.php
+++ users/SongPham/cfyc/pages/home.php
@@ -262,19 +262,19 @@
                         <div class="row">
                             <div class="col-md-2 hide-sm hide-xs text-center">
                             </div>
-                            <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                            <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                 <img src="img/home/california-wow-celeb-member-1.png" alt="Celebrities member" title="Celebrities member">
                                 <p>Ho Ngoc Ha</p>
                             </div>
-                            <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                            <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                 <img src="img/home/california-wow-celeb-member-2.png" alt="Celebrities member" title="Celebrities member">
                                 <p>Hoang My</p>
                             </div>
-                            <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                            <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                 <img src="img/home/california-wow-celeb-member-3.png" alt="Celebrities member" title="Celebrities member">
                                 <p>Ho Vinh Khoa</p>
                             </div>
-                            <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                            <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                 <img src="img/home/california-wow-celeb-member-4.png" alt="Celebrities member" title="Celebrities member">
                                 <p>Thuy Trang</p>
                             </div>
@@ -285,19 +285,19 @@
                              <div class="whitespace"></div>
                                 <div class="col-md-2 hide-sm hide-xs text-center">
                                 </div>
-                                <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                                <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                     <img src="img/home/california-wow-celeb-member-1.png" alt="Celebrities member" title="Celebrities member">
                                     <p>Ho Ngoc Ha</p>
                                 </div>
-                                <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                                <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                     <img src="img/home/california-wow-celeb-member-2.png" alt="Celebrities member" title="Celebrities member">
                                     <p>Hoang My</p>
                                 </div>
-                                <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                                <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                     <img src="img/home/california-wow-celeb-member-3.png" alt="Celebrities member" title="Celebrities member">
                                     <p>Ho Vinh Khoa</p>
                                 </div>
-                                <div class="col-md-2 col-sm-3 col-xs-6 text-center">
+                                <div class="col-md-2 col-sm-3 col-xs-6 text-center celeb-items">
                                     <img src="img/home/california-wow-celeb-member-4.png" alt="Celebrities member" title="Celebrities member">
                                     <p>Thuy Trang</p>
                                 </div>
