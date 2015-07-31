09a386442ff7e22d89dc2c6cebd8f07cc4c9b8bd
Thai Anh <root>
2015-07-31 17:28:06 +0700

    Club page layout+css

---
+++ users/SongPham/cfyc/public/js/scripts/club.js
@@ -0,0 +1,14 @@
+var clubPage = (function () {
+    var initModule = function () {
+
+        $(document).ready(function($){
+
+        });
+
+
+    };
+    return {
+        initModule: initModule
+    };
+}());
+clubPage.initModule();
