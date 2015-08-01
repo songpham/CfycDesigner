24ea8d090d93b06b2f5dd3b275ff44f0094656bd
songpham <song20002005@yahoo.com>
2015-08-01 11:23:58 +0700

    add js file

---
+++ users/SongPham/cfyc/public/js/scripts/centuryon-memberships.js
@@ -0,0 +1,11 @@
+var centuryonMembershipsPage = (function () {
+    var initModule = function () {
+        $(document).ready(function($){
+            /* code at here */
+        });
+    };
+    return {
+        initModule: initModule
+    };
+}());
+centuryonMembershipsPage.initModule();
