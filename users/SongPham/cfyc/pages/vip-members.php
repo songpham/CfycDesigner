92f63120f8a834ef7004d1fd682eddc6176763fb
boducoscar <amthucgiaitri@gmail.com>
2015-08-01 17:41:49 +0700

    finish vip visit page

---
+++ users/SongPham/cfyc/pages/vip-members.php
@@ -0,0 +1,152 @@
+<div class="container-fluid page vip-members-page">
+<?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
+<div class="container-fluid">
+    <!-- Top Banner -->
+    <div class="row gallery">
+    	<a href="javascript:void(0)" title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
+    		<img src="img/vip-members/california-wow-vip-members.jpg"  title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
+    	</a>
+    </div><!-- /.Top Banner -->
+    <div class="row top-content">
+        <div class="holder-top-content">
+        	<div class="whitespace"></div>
+            <div class="col-md-12 text-center">
+                <div class="row rsvpnow">
+                	<div class="hidden-md col-lg-2 hidden-sm hidden-xs text-center"></div>
+                    <div class="col-md-12 col-lg-8 col-sm-12 col-xs-12 text-center">
+						<h3 class="section_title">
+							For over 7 years California Fitness has made it its mission to make life better as a place where fitness, fashion and entertainment merge in a healthy, high energy environment to deliver a dynamic lifestyle and life enhancement experience.<br/><br/>
+							RSVP now for a complementary VIP visit at any California Fitness location of your choice including:
+						</h3>
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs"></div>
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<hr></hr>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs"></div>
+						</div>
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<img src="img/vip-members/california-wow-vip-member-1.jpg" alt="Exclusive 1 on 1 consultation" title="Exclusive 1 on 1 consultation">
+								<h2 class="title">Exclusive 1 on 1 consultation</h2>
+								<p class="title_desc">Learn about life enhancement services and how you can reach your goals to lose weight, build muscle and much more.</p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<img src="img/vip-members/california-wow-vip-member-2.jpg" alt="InBody Assessment" title="InBody Assessment">
+								<h2 class="title">InBody Assessment</h2>
+								<p class="title_desc">Let our experts help you learn more about your body and identify the best solutions, personalized exactly for you, your needs and your goals.</p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<img src="img/vip-members/california-wow-vip-member-3.jpg" alt="Personal Training" title="Personal Training">
+								<h2 class="title">Personal Training</h2>
+								<p class="title_desc">Don’t just learn how to reach your goals, but get started with an exclusive 1 on 1 personal training orientation.</p>
+							</div>
+						</div>
+						<h3 class="section_title">
+							And have a chance to experience over 20 other life enhancement services at california fitness & yoga including:
+						</h3>
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs"></div>
+							<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+								<hr></hr>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs"></div>
+						</div>
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-1.jpg" alt="Yoga" title="Yoga">
+								<div class="overlay"><h2 class="title">Yoga</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-2.jpg" alt="Dance" title="Dance">
+								<div class="overlay"><h2 class="title">Dance</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-3.jpg" alt="Group Fitness" title="Group Fitness">
+								<div class="overlay"><h2 class="title">Group Fitness</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-4.jpg" alt="Kick Fit" title="Kick Fit">
+								<div class="overlay"><h2 class="title">Kick Fit</h2></div>								
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-5.jpg" alt="Huấn luyện cá nhân" title="Huấn luyện cá nhân">
+								<div class="overlay"><h2 class="title">Huấn luyện cá nhân</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-6.jpg" alt="Giảm căng cơ" title="Giảm căng cơ">
+								<div class="overlay"><h2 class="title">Giảm căng cơ</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-7.jpg" alt="Võ tự do MMA" title="Võ tự do MMA">
+								<div class="overlay"><h2 class="title">Võ tự do MMA</h2></div>								
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 chance_items">
+								<img src="img/vip-members/california-wow-chance-to-experience-8.jpg" alt="PTX 2" title="PTX 2">
+								<div class="overlay"><h2 class="title">PTX<sup>2</sup></h2></div>								
+							</div>
+						</div>
+						<div class="whitespace"></div>
+            			<div class="whitespace"></div>
+            			<div class="row ">
+            				<div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"></div>
+                    		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                    			<h2 class="booktitle">book a vip visit today</h3>
+								<h3 class="visit_benefit">
+								<span class="title">SIGN UP FOR A FREE VISIT INCLUDING</span><br/><br/>
+								<strong>- EXCLUSIVE TOUR</strong><br/>
+								<span>(Learn about all features, programs and equipment available)</span><br/><br/>
+								<strong>- PT FITNESS CONSULTATION</strong><br/>
+								<span>(Learn more about your body and how to reach your goals with our fitness experts)</span><br/><br/>
+								<strong>- FULL DAY ACCESS</strong><br/>
+								<span>(One day full access pass to experience our clubs and programs)</span><br/><br/>
+								<span>fill in the form and WE WILL CONTACT YOU WITHIN 24 HOURS</span>
+								</h3>
+								<div class="row registerfrm text-left">
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<label>Full name:</label>
+										<p><input type="text" placeholder="Full Name" name="full_name"></p>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<label>Phone number:</label>
+										<p><input type="text" placeholder="Phone Number" name="phone_number"></p>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<label>Email:</label>
+										<p><input type="text" placeholder="Email Address" name="email_add"></p>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<label>Choose your nearest club:</label>
+										<p>
+											<select name="nearest_club">
+												<option>Choose a club</option>
+												<option>HCMC DISTRICT 1</option>
+												<option>HCMC DISTRICT 4</option>
+												<option>HCMC DISTRICT 5</option>
+											</select>
+										</p>
+									</div>
+									<div class="row">
+										<div class="col-lg-3 col-md-3 col-sm-3 col-xs-1"></div>
+										<div class="col-lg-6 col-md-6 col-sm-6 col-xs-10">
+											<input type="submit" class="submitbtn" value="book a vip visit">
+										</div>
+										<div class="col-lg-3 col-md-3 col-sm-3 col-xs-1"></div>
+									</div>
+								</div>
+                    		</div>
+                    		<div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"></div>
+                		</div>
+                    </div>
+                    <div class="hidden-md col-lg-2 hidden-sm hidden-xs text-center"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/vip-members.js"></script>
