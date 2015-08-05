04f52c41b9ea1d664625365e9bc7168d5a8f829f
Glen <glencoffey@cfyc.com.vn>
2015-08-05 17:59:14 +0700

    Centuryon Language + Adjustments

---
+++ users/SongPham/cfyc/languages/vn/centuryon-membership.php
@@ -0,0 +1,37 @@
+<?php
+
+define('LANG_CENTURYON_MEMBERSHIPS', 'centuryon_memberships');
+
+define('LANG_CENTURYON_MEMBERSHIPS_BANNER_TITLE', 'Khám phá Centuryon');
+define('LANG_CENTURYON_MEMBERSHIPS_BANNER_SUBTEXT', 'Tích hợp mọi dịch vụ ưu đãi, độc quyền và đứng đầu các thứ hạng hội viên, thẻ hội viên Centuryon mang đến cho bạn những ưu đãi tuyệt vời nhất.');
+define('LANG_CENTURYON_MEMBERSHIPS_BANNER_CTA', 'Get A Trial');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_1_VIDEO_TEXT', 'Đó là một sự hợp tác lâu dài, vượt trội về sự cam kết cải thiện chất lượng cuộc sống của chính bạn. Với thẻ hội viên độc đáo này, bạn sẽ được tận hưởng môi trường thật sự giá trị, thoải mái và giúp cải thiện lối sống; cho phép bạn đạt được cuộc sống chất lượng nhất.');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_2_MAIN_TITLE_1', 'Cải thiện cuộc sống và hơn thế nữa');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_2_MAIN_TITLE_1_TEXT', ' Trải nghiệm một đẳng cấp mới để cải thiện và nâng cao chất lượng cuộc sống của bạn. Tất cả các hội viên Centuryon được sử dụng trọn gói dịch vụ huấn luyện cá nhân 1-1, một nhóm huấn luyện viên chuyên chuyên nghiệp dành riêng cho Centuryon cam kết giúp bạn vươn tới mục tiêu cao nhất; trao cho bạn niềm tin để đạt được mục tiêu và có một cuộc sống chất lượng hơn.');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_3_MAIN_TITLE_1', 'Centuryon được thiết kế để đáp ứng nhu cầu của những ai không chấp nhận bất kỳ điều gì ngoài sự tốt nhất');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_3_QUOTE','Ngoài những thiết bị tập luyện và dịch vụ cá nhân, Centuryon còn cung cấp nhiều hơn nữa');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_IMG1_DESC','Kết nối với những người quan trọng trong VIP Centuryon Lounge');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_IMG2_DESC','Tận hưởng sự sang trọng trong phòng tắm');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_IMG3_DESC','Tận hưởng trang phục độc quyền Centuryon Athletica');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_MAIN_TITLE1','Chất lượng vượt trội từ những điều nhỏ nhất');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE1','Mọi nơi bạn ghé ngang đều được tỉ mỉ sắp đặt để đảm bảo bạn có được trải nghiệm tuyệt vời nhất mỗi lần bạn đến:');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE2','Đặt chỗ trước ở mọi lớp:');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE2_TEXT',' Không phải chờ hay bỏ lỡ bất kỳ lớp học yêu thích nào của bạn.');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE3','Khăn VIP và áo choàng tắm:');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE3_TEXT','Được cung cấp mỗi lần bạn sử dụng dịch vụ bể bơi, phòng tắm hơi sau khi tập luyện');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE4','Được cung cấp thức uống dinh dưỡng: ');
+define('LANG_CENTURYON_MEMBERSHIPS_ID_4_SUB_TITLE4_TEXT',' Món sinh tố hoặc thức uống bạn thích nhất có sẵn mỗi lần bạn đến');
+
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_5_QUOTE','Centuryon cho phép bạn rủ theo bạn bè hoặc người thân trong gia đình đến cùng mình mỗi lần đi tập, giúp bạn có thêm niềm vui và cải thiện chất lượng cuộc sống của những người xung quanh bạn với đặc quyền không giới hạn của khách VIP. <br><br> Mọi nơi bạn ghé ngang đều đã được sắp đặt một cách tỉ mỉ, từ việc đặt chỗ trước ở mọi lớp đến khăn VIP/áo choàng tắm - tận hưởng khu vực độc quyền cho phép bạn cải thiện cuộc sống theo cách bạn muốn.');
+
+define('LANG_CENTURYON_MEMBERSHIPS_ID_6_MAIN_TITLE1','Bạn sẽ đến đâu để thực sự được trẻ hoá?');
+
+
+?>
