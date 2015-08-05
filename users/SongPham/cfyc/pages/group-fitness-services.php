4f54c62e5e8a579fb14f5dd09e2b138c125180f7
pinkybee277 <pinkybee277@gmail.com>
2015-08-05 09:29:21 +0700

    add kickfit service and pt services

---
+++ users/SongPham/cfyc/pages/group-fitness-services.php
@@ -0,0 +1,265 @@
+<div class="container-fluid page dance-services-page">
+    <?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
+    <div class="top-content row">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/dance-services.jpg" alt="Dance Services">
+                    <div class="carousel-caption">
+                        <fieldset>
+                            <legend>Discover</legend>
+                            the beauty & excitement of Cali Dance<br>
+                            <span>Experience a dance class today</span>
+                            <a class="cta" href="#" title="Discover Cali Dance">Try a class</a>
+                        </fieldset>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/dance-services.jpg" alt="Dance Services">
+                    <div class="carousel-caption">
+                        <fieldset>
+                            <legend>Discover</legend>
+                            the beauty & excitement of Cali Dance<br>
+                            <span>Experience a dance class today</span>
+                            <a class="cta" href="#" title="Discover Cali Dance">Try a class</a>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="clearfix"></div>
+    <div class="calidance container">
+        <div class="row">
+            <h2><div class="line"></div>about calidance<div class="line"></div></h2>
+            <div class="col-lg-offset-1 col-md-offset-1 col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                <p>Pick and choose from an extensive variety of cutting-edge classes that will energize your movements
+                    with the rhythm of exhilarating music. Workout with the movements you love best!</p>
+                <p>Dance your way to cardio fitness with BodyJam.Ride your cycling workout with RPM. Unleash a fusion of
+                    cardio workout and martial arts with BodyCombat.</p>
+                <p>Power up every muscle in your body with the barbell class BodyPump. Party your way to fitness with
+                    the red-hot, calorie-burning moves of Zumba and Latin Dance. Delight in the exotic body movements of
+                    the Belly Dance. Follow your desire!</p>
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" height="200" src="https://www.youtube.com/embed/Ol1nuRXaVcM" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="benefits">
+        <h2><div class="line"></div>Benefits of Dance<div class="line"></div></h2>
+        <p>Dance provides people with a more fast paced and exciting variety of classes to help you engage with other people who have similar goals in a fun and healthy way.</p>
+        <p></p>
+        <p>All memberships at California Fitness & Yoga centers come with unlimited access to all dance classes</p>
+        <p></p>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 cover">
+            <a href="#" title="Dance is fun">
+                <img class="img-responsive" src="<?php echo USER_BASE_URL ?>/img/dance-services/benefit1.jpg" alt="Dance Services">
+            </a>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 cover">
+            <a href="#" title="Your environment">
+                <img class="img-responsive" src="<?php echo USER_BASE_URL ?>/img/dance-services/benefit2.jpg" alt="Dance Services">
+            </a>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="discover row">
+        <h2><div class="line"></div>Discover dance at California Fitness<div class="line"></div></h2>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active">
+                <a href="#zumba" aria-controls="zumba" role="tab" data-toggle="tab">ZUMBA (M)</a>
+            </li>
+            <li role="presentation">
+                <a href="#bodyjam" aria-controls="bodyjam" role="tab" data-toggle="tab">BODY JAM (M)</a>
+            </li>
+            <li role="presentation">
+                <a href="#sexydance" aria-controls="sexydance" role="tab" data-toggle="tab">SEXY DANCE (M)</a>
+            </li>
+            <li role="presentation">
+                <a href="#poledance" aria-controls="poledance" role="tab" data-toggle="tab">POLE DANCE (M)</a>
+            </li>
+            <li role="presentation">
+                <a href="#salsa" aria-controls="salsa" role="tab" data-toggle="tab">SALSA & LATIN DANCE (M)</a>
+            </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="zumba">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/discover.jpg" alt="Dance Services">
+                            <div class="carousel-caption">
+                                <h3>zumba</h3>
+                                <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
+                                <p> The ZUMBA program fuses hypnotic Latin rhythms and easy-to-follow moves to
+                                    create a one-of-a-kind fitness program that will blow you away.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="bodyjam">bodyjam</div>
+            <div role="tabpanel" class="tab-pane" id="sexydance">sexydance</div>
+            <div role="tabpanel" class="tab-pane" id="poledance">poledance</div>
+            <div role="tabpanel" class="tab-pane" id="salsa">salsa</div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="school row">
+        <h2><div class="line"></div>THE PERFECT ENVIRONMENT FOR YOU TO SUCCEED<div class="line"></div></h2>
+        <div id="carousel-example-generic-clubs" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic-clubs" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic-clubs" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic-clubs" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic-clubs" data-slide-to="3"></li>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/school.jpg" alt="School">
+                </div>
+                <div class="item">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/school.jpg" alt="School">
+                </div>
+                <div class="item">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/school.jpg" alt="School">
+                </div>
+                <div class="item">
+                    <img src="<?php echo USER_BASE_URL ?>/img/dance-services/school.jpg" alt="School">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="trainers container">
+        <h2><div class="line"></div>Meet our featured dance teachers<div class="line"></div></h2>
+        <center>You have the potential, we can help you reach it.</center>
+        <ul class="nav nav-tabs area" role="tablist">
+            <li role="presentation" class="active">
+                <a href="#hcmc" aria-controls="hcmc" role="tab" data-toggle="tab">HCMC</a>
+            </li>
+            <li role="presentation">
+                <a href="#hanoi" aria-controls="hanoi" role="tab" data-toggle="tab">HA NOI</a>
+            </li>
+            <li role="presentation">
+                <a href="#danang" aria-controls="danang" role="tab" data-toggle="tab">DA NANG</a>
+            </li>
+            <li role="presentation">
+                <a href="#binhduong" aria-controls="binhduong" role="tab" data-toggle="tab">BINH DUONG</a>
+            </li>
+            <li role="presentation">
+                <a href="#bienhoa" aria-controls="bienhoa" role="tab" data-toggle="tab">BIEN HOA</a>
+            </li>
+        </ul>
+        <div class="tab-content teacher-detail">
+            <div role="tabpanel" class="tab-pane active" id="hcmc">
+                <ul class="nav nav-tabs col-lg-2 col-md-2 col-sm-2 col-xs-12" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#trainer1" aria-controls="trainer1" role="tab" data-toggle="tab">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/dj-icon.png" alt="DJ">
+                            <span>DJ</span>
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#trainer2" aria-controls="trainer2" role="tab" data-toggle="tab">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/tylerly-icon.png" alt="DJ">
+                            <span>TYLER LY</span>
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#trainer3" aria-controls="trainer3" role="tab" data-toggle="tab">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/genesis-icon.png" alt="DJ">
+                            <span>GENESIS</span>
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#trainer4" aria-controls="trainer4" role="tab" data-toggle="tab">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/katsu-icon.png" alt="DJ">
+                            <span>KATSU</span>
+                        </a>
+                    </li>
+                </ul>
+                <div class="tab-content col-lg-10 col-md-10 col-sm-10 col-xs-12">
+                    <div role="tabpanel" class="tab-pane active" id="trainer1">
+                        <div class="col-lg-5 col-md-5 col-sm-6 col-sx-12" style="text-align: center;">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/dj-photo.jpg" alt="DJ">
+                        </div>
+                        <div class="col-lg-7 col-md-7 col-sm-6 col-sx-12 cont">
+                            <h3>DJ</h3>
+                            <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
+                            <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
+                            <p></p>
+                            <p>Bio:<br>
+                            DJ đã say mê luyện tập thể hình khi khi mới lên 5 tuổi. Người thầy đầu tiên chính là cha anh, một võ sư, đã hướng dẫn DJ luyện tập miệt mài để đạt mục tiêu, để tiếp tục vươn tới mỗi khi nản lòng, luôn khiêm nhường trong chiến thắng và không bao giờ bỏ cuộc. DJ đã từng là nhà vô địch giải trẻ và thẳng tiến tới chức vô địch quốc gia Võ Cổ Truyền Việt Nam.
+                            </p>
+                            <p></p>
+                            <p>DJ phát hiện ra niềm đam mê nhảy múa khi đến tuổi 15. Anh đã trải qua hơn 7 năm kinh nghiệm luyện tập vũ đạo và biểu diễn với tư cách là một vũ công hip-hop. DJ luôn say sưa luyện tập, tự thách thức bản thân và khám phá khả năng của mình trong thể dục và thể thao. Lý tưởng sống của anh là tiếp tục học các phong cách nhảy múa khác và luôn phấn đấu trở thành một vũ công xuất sắc.</p>
+                        </div>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="trainer2">
+                        <div class="col-lg-5 col-md-5 col-sm-6 col-sx-12" style="text-align: center;">
+                            <img class="img-responsive"
+                                 src="<?php echo USER_BASE_URL ?>/img/dance-services/dj-photo.jpg" alt="DJ">
+                        </div>
+                        <div class="col-lg-7 col-md-7 col-sm-6 col-sx-12 cont">
+                            <h3>TYLER LY</h3>
+                            <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
+                            <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
+                            <p></p>
+                            <p>Bio:<br>
+                                DJ đã say mê luyện tập thể hình khi khi mới lên 5 tuổi. Người thầy đầu tiên chính là cha anh, một võ sư, đã hướng dẫn DJ luyện tập miệt mài để đạt mục tiêu, để tiếp tục vươn tới mỗi khi nản lòng, luôn khiêm nhường trong chiến thắng và không bao giờ bỏ cuộc. DJ đã từng là nhà vô địch giải trẻ và thẳng tiến tới chức vô địch quốc gia Võ Cổ Truyền Việt Nam.
+                            </p>
+                            <p></p>
+                            <p>DJ phát hiện ra niềm đam mê nhảy múa khi đến tuổi 15. Anh đã trải qua hơn 7 năm kinh nghiệm luyện tập vũ đạo và biểu diễn với tư cách là một vũ công hip-hop. DJ luôn say sưa luyện tập, tự thách thức bản thân và khám phá khả năng của mình trong thể dục và thể thao. Lý tưởng sống của anh là tiếp tục học các phong cách nhảy múa khác và luôn phấn đấu trở thành một vũ công xuất sắc.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div role="tabpanel" class="tab-pane" id="trainer3">TRAINER 3</div>
+            <div role="tabpanel" class="tab-pane" id="trainer4">TRAINER 4</div>
+            <div role="tabpanel" class="tab-pane" id="trainer5">TRAINER 5</div>
+            <div role="tabpanel" class="tab-pane" id="trainer6">TRAINER 6</div>
+        </div>
+
+
+    </div>
+    <div class="clearfix"></div>
+
+    <div class="contactform container">
+        <div class="col-lg-offset-3 col-md-offset-3 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h2 class="title">try a dance session</h2>
+            <h3>SIGN UP TO TRY A COMPLEMENTARY DANCE CLASSES!</h3>
+            <p>INCLUDING:<br>
+                - FITNESS ASSESSMENT & CONSULTATION<br>
+                - ONE DAY ACCESS TO CALIFORNIA FITNESS & YOGA CENTERS
+            </p>
+            <br>
+            <p>fill in the form and WE WILL CONTACT YOU WITHIN 24 HOURS</p>
+        </div>
+
+    </div>
+
+    <?php include USER_BASE_PATH . '/templates/footer-bar.php'; ?>
+</div>
+<?php include USER_BASE_PATH . '/templates/footer.php'; ?>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/dance-services-top.js"></script>
+<script src="<?php echo USER_BASE_URL ?>/js/scripts/dance-services-bottom.js"></script>
+
