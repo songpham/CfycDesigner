35eed39650643013405746bcc071c3d4103f578b
songpham <song20002005@yahoo.com>
2015-08-08 17:35:24 +0700

    fix bugs language

--- users/SongPham/cfyc/public/index.php
+++ users/SongPham/cfyc/public/index.php
@@ -4,6 +4,46 @@
 <?php include USER_BASE_PATH . '/templates/head.php'; ?>
 <body>
 <?php
+function getFullUrl () {
+    $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+    return $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+}
+function parseArrayUri ($suffix) {
+    $parse = array();
+    if (empty($suffix) === FALSE) {
+        $array = explode('&', $suffix);
+        if (empty($array) === FALSE) {
+            foreach ($array as $item) {
+                $arr = explode('=', $item);
+                $parse[$arr[0]] = $arr[1];
+            }
+        }
+    }
+    return $parse;
+}
+function getSuffix ($suffix, $url = NULL, $defaultLanguage = DEFAULT_LANGUAGE) {
+    $url = empty($url) === FALSE ? $url : rtrim(substr($_SERVER['REQUEST_URI'], strlen(USER_BASE_URL) + 1), '/');
+    $uri = explode('/', $url);
+    $endUri = end($uri);
+    $result = NULL;
+    if (strpos($endUri, '?') === 0) {
+        $endUriParsed = array_merge(parseArrayUri(ltrim($endUri, '?')), parseArrayUri($suffix));
+        $newSuffix = array();
+        if (empty($endUriParsed) === FALSE) {
+            foreach ($endUriParsed as $k => $v) {
+                $newSuffix[] = $k . '=' . $v;
+            }
+            $result = '/?' . implode('&', $newSuffix);
+        }
+    } else {
+        $result = '/?' . $suffix;
+    }
+    $result = str_replace(array('&lang=' . $defaultLanguage, '?lang=' . $defaultLanguage, '/?&'), array(NULL, '?', '/?'), $result);
+    if ($result === '/?') {
+        $result = NULL;
+    }
+    return $result;
+}
 function debug ($var, $traceDetail = FALSE) {
     echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
     // var_dump the variable into a buffer and keep the output
@@ -35,10 +75,14 @@ function debug ($var, $traceDetail = FALSE) {
             . '</pre>';
     echo $output;
 }
+$LANG = DEFAULT_LANGUAGE;
 if (empty($_GET['lang']) === FALSE) {
     $LANG = $_GET['lang'];
-} else {
-    $LANG = 'vn';
+}
+if ($LANG == 'vn') {
+    $OPPOSITE_LANG = 'en';
+} else if ($LANG == 'en') {
+    $OPPOSITE_LANG = 'vn';
 }
 /**
  * include languages files
@@ -56,9 +100,9 @@ if (empty($languageFiles) === FALSE) {
         }
     }
 }
-$uri = str_replace(strtolower(USER_BASE_URL . '/'), NULL, strtolower($_SERVER['REQUEST_URI']));
-if (is_file(USER_BASE_PATH . '/pages/' . $uri . '.php') === TRUE) {
-    include USER_BASE_PATH . '/pages/' . $uri . '.php';
+$URI = rtrim(preg_replace('/\\?.*/', '', str_replace(strtolower(USER_BASE_URL . '/'), NULL, strtolower($_SERVER['REQUEST_URI']))), '/');
+if (is_file(USER_BASE_PATH . '/pages/' . $URI . '.php') === TRUE) {
+    include USER_BASE_PATH . '/pages/' . $URI . '.php';
 } else {
     include USER_BASE_PATH . '/pages/home.php';
 }
