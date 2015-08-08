498d27a59e21d00beccb277b6f856ab4144848ca
boducoscar <amthucgiaitri@gmail.com>
2015-08-08 17:10:04 +0700

    update home & vip visit

--- users/SongPham/cfyc/templates/nav-bar.php
+++ users/SongPham/cfyc/templates/nav-bar.php
@@ -1,70 +1,72 @@
 <!-- Mobile Navigation -->
-<a id="mobile-navigation-menu-toggle" href="#" class="btn btn-dark btn-lg toggle">
+<a id="mobile-navigation-menu-toggle" href="javascript:void(0)" class="btn btn-dark btn-lg toggle">
     <i class="fa fa-bars"></i>
 </a>
 <nav id="mobile-navigation-wrapper">
     <ul class="sidebar-nav">
-        <a id="mobile-navigation-menu-close" href="#" class="btn btn-light btn-lg pull-right toggle">
+        <a id="mobile-navigation-menu-close" href="javascript:void(0)" class="btn btn-light btn-lg pull-right toggle">
             <i class="fa fa-times"></i>
         </a>
         <li class="sidebar-brand">
-             <a href="#language"><img src="<?php echo USER_BASE_URL ?>/img/english.png" class="language"></a>
+             <a href="javascript:void(0)"><img src="<?php echo USER_BASE_URL ?>/img/english.png" class="language"></a>
         </li>
         <li>
-            <a href="#">Trang Chủ</a>
+            <a href="<?php echo USER_BASE_URL ?>/">Trang Chủ</a>
         </li>
         <li>
             <div class="list-group panel">
-                <a href="#club-menu" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Câu Lạc Bộ<span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a href="javascript:void(0)" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Câu Lạc Bộ<span class="glyphicon glyphicon-chevron-right"></span></a>
                 <div class="collapse" aria-expanded="false">
                     <ul>
-                        <li><a href="#">TP. Hồ Chí Minh</a></li>
-                        <li><a href="#">Hà Nội</a></li>
-                        <li><a href="#">Đà Nẵng</a></li>
-                        <li><a href="#">Bình Dương</a></li>
-                        <li><a href="#">Biên Hòa</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/club-hcm">TP. Hồ Chí Minh</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/club-ha-noi">Hà Nội</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/club-da-nang">Đà Nẵng</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/club-binh-duong">Bình Dương</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/club-bien-hoa">Biên Hòa</a></li>
                     </ul>
                 </div>
             </div>
         </li>
         <li>
             <div class="list-group panel">
-                <a href="#dich-vu-menu" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Dịch Vụ <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a href="javascript:void(0)" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Dịch Vụ <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <div class="collapse" aria-expanded="false">
                     <ul>
-                        <li><a href="#">Group Fitness</a></li>
-                        <li><a href="#">Giảm Căng Cơ</a></li>
-                        <li><a href="#">Dance</a></li>
-                        <li><a href="#">Yoga</a></li>
-                        <li><a href="#">Huấn Luyện cá nhân</a></li>
-                        <li><a href="#">Kickfit</a></li>
-                        <li><a href="#">Võ tự do MMA</a></li>
-                        <li><a href="#">Spa</a></li>
-                        <li><a href="#">Clb dành cho trẻ em</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/group-fitness-services">Group Fitness</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/dynamicstretching-services">Giảm Căng Cơ</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/dance-services">Dance</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/yoga-services">Yoga</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/pt-services">Huấn Luyện cá nhân</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/kickfit-mma-services">Kickfit</a></li>
+                        <li><a href="<?php echo USER_BASE_URL ?>/kickfit-mma-services">Võ tự do MMA</a></li>
                     </ul>
                 </div>
             </div>
         </li>
         <li>
             <div class="list-group panel">
-                <a href="#lifestyle-menu" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Phong Cách Sống<span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a href="javascript:void(0)" class="list-group-item-success" data-toggle="collapse" aria-expanded="false">Phong Cách Sống<span class="glyphicon glyphicon-chevron-right"></span></a>
                 <div class="collapse" aria-expanded="false">
                     <ul>
-                        <li><a href="#">Giảm Cân</a></li>
-                        <li><a href="#">Thể dục & Sức khỏe</a></li>
-                        <li><a href="#">Luyện Tập</a></li>
-                        <li><a href="#">Phong Cách Sống</a></li>
-                        <li><a href="#">Dinh Dưỡng</a></li>
-                        <li><a href="#">Gia Đình</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/giam-can-cap-toc/">Giảm Cân</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/the-duc-suc-khoe/">Thể dục & Sức khỏe</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/luyen-tap/">Luyện Tập</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/phong-cach-song/">Phong Cách Sống</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/dinh-duong/">Dinh Dưỡng</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/gia-dinh/">Gia Đình</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/yoga/">Yoga</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/thoi-trang/">Thời Trang</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/tin-tuc/">Tin Tức</a></li>
+                        <li><a href="http://lifestyle.cfyc.com.vn/su-kien/">Sự Kiện</a></li>
                     </ul>
                 </div>
             </div>
         </li>
         <li>
-            <a href="#" onclick = $("#mobile-navigation-menu-close").click(); >Thành Viên</a>
+            <a href="<?php echo USER_BASE_URL ?>/cfyc-memberships" onclick = $("#mobile-navigation-menu-close").click(); >Thành Viên</a>
         </li>
         <li>
-            <a href="#" onclick = $("#mobile-navigation-menu-close").click(); >Lịch học</a>
+            <a href="<?php echo USER_BASE_URL ?>/classes" onclick = $("#mobile-navigation-menu-close").click(); >Lịch học</a>
         </li>
     </ul>
 </nav>
@@ -76,45 +78,49 @@
         <img src="<?php echo USER_BASE_URL ?>/img/logo.png" class="img-responsive logo-img" alt="California Fitness &amp; Yoga">
     </a>
     <ul class="desktop-menu">
-        <li><a href="#"><?php echo LANG_HOMEPAGE ?></a></li>
-        <li><a href="#" data-index="0"><?php echo LANG_CLUBS ?></a></li>
-        <li><a href="#" data-index="1">Dịch Vụ</a></li>
-        <li><a href="#" data-index="2">Phong Cách Sống</a></li>
-        <li><a href="#">Thành Viên</a></li>
-        <li><a href="#">Lịch học</a></li>
-        <li><a href="#"><img src="<?php echo USER_BASE_URL ?>/img/english.png" class="language"></a></li>
+        <li><a href="<?php echo USER_BASE_URL ?>"><?php echo LANG_HOMEPAGE ?></a></li>
+        <li><a href="javascript:void(0)" data-index="0"><?php echo LANG_CLUBS ?></a></li>
+        <li><a href="javascript:void(0)" data-index="1">Dịch Vụ</a></li>
+        <li><a href="javascript:void(0)" data-index="2">Phong Cách Sống</a></li>
+        <li><a href="<?php echo USER_BASE_URL ?>/cfyc-memberships">Thành Viên</a></li>
+        <li><a href="<?php echo USER_BASE_URL ?>/classes">Lịch học</a></li>
+        <li><a href="javascript:void(0)"><img src="<?php echo USER_BASE_URL ?>/img/english.png" class="language"></a></li>
     </ul>
     <div class="holder-submenu">
         <div class="sub-menu collapse">
             <ul>
-                <li><a href="#"><?php echo LANG_CLUB_HCM ?></a></li>
-                <li><a href="#"><?php echo LANG_CLUB_HA_NOI ?></a></li>
-                <li><a href="#"><?php echo LANG_CLUB_DA_NANG ?></a></li>
-                <li><a href="#"><?php echo LANG_CLUB_BINH_DUONG ?></a></li>
-                <li><a href="#"><?php echo LANG_CLUB_BIEN_HOA ?></a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/club-hcm"><?php echo LANG_CLUB_HCM ?></a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/club-ha-noi"><?php echo LANG_CLUB_HA_NOI ?></a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/club-da-nang"><?php echo LANG_CLUB_DA_NANG ?></a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/club-binh-duong"><?php echo LANG_CLUB_BINH_DUONG ?></a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/club-bien-hoa"><?php echo LANG_CLUB_BIEN_HOA ?></a></li>
             </ul>
         </div>
         <div class="sub-menu collapse">
             <ul>
-                <li><a href="#">Group Fitness</a></li>
-                <li><a href="#">Giảm Căng Cơ</a></li>
-                <li><a href="#">Dance</a></li>
-                <li><a href="#">Yoga</a></li>
-                <li><a href="#">Huấn Luyện cá nhân</a></li>
-                <li><a href="#">Kickfit</a></li>
-                <li><a href="#">Võ tự do MMA</a></li>
-                <li><a href="#">Spa</a></li>
-                <li><a href="#">Clb dành cho trẻ em</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/group-fitness-services">Group Fitness</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/dynamicstretching-services">Giảm Căng Cơ</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/dance-services">Dance</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/yoga-services">Yoga</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/pt-services">Huấn Luyện cá nhân</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/kickfit-mma-services">Kickfit</a></li>
+                <li><a href="<?php echo USER_BASE_URL ?>/kickfit-mma-services">Võ tự do MMA</a></li>
+               <!-- <li><a href="javascript:void(0)#">Spa</a></li>
+                <li><a href="javascript:void(0)">Clb dành cho trẻ em</a></li> -->
             </ul>
         </div>
         <div class="sub-menu collapse">
             <ul>
-                <li><a href="#">Giảm Cân</a></li>
-                <li><a href="#">Thể dục & Sức khỏe</a></li>
-                <li><a href="#">Luyện Tập</a></li>
-                <li><a href="#">Phong Cách Sống</a></li>
-                <li><a href="#">Dinh Dưỡng</a></li>
-                <li><a href="#">Gia Đình</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/giam-can-cap-toc/">Giảm Cân</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/the-duc-suc-khoe/">Thể dục & Sức khỏe</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/luyen-tap/">Luyện Tập</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/phong-cach-song/">Phong Cách Sống</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/dinh-duong/">Dinh Dưỡng</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/gia-dinh/">Gia Đình</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/yoga/">Yoga</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/thoi-trang/">Thời Trang</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/tin-tuc/">Tin Tức</a></li>
+                <li><a href="http://lifestyle.cfyc.com.vn/su-kien/">Sự Kiện</a></li>
             </ul>
         </div>
     </div>
