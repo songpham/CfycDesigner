ac1c37668160ae8bfc392dd32f73842f575ccaa1
Glen <glencoffey@cfyc.com.vn>
2015-08-09 23:55:07 +0700

    About Us Update

--- users/SongPham/cfyc/pages/about-us.php
+++ users/SongPham/cfyc/pages/about-us.php
@@ -2,31 +2,29 @@
 <?php include USER_BASE_PATH . '/templates/nav-bar.php'; ?>
     <div class="row banner-top">
         <img src="<?php echo USER_BASE_URL ?>/img/about-us/bg-header.jpg" class="img-responsive banner" onmousedown="return false;"/>
-        <h1 class="headline"><span>CALIFORNIA<br/>FITNESS & YOGA</span><br/>IN VIETNAM</h1>
+        <h1 class="headline"><?php echo LANG_ABOUT_US_MAIN_TITLE ?></h1>
     </div>
     <div class="container-page">
         <div class="row quotes-block">
             <div class="col-sm-12">
-                <span class="open-quotes">&ldquo;</span><h3>OUR GOAL IS TO SHOW PEOPLE THAT LIVING BETTER AND HEALTHIER IS NOT ABOUT DISCIPLINE AND SACRIFICE, BUT INSTEAD IS ABOUT INTEGRATING THOSE THINGS THAT ENHANCE YOUR LIFE AND ELIMINATING THE THINGS THAT DON’T.</h3><span class="close-quotes">&rdquo;</span>
+                <span class="open-quotes">&ldquo;</span><h3><?php echo LANG_ABOUT_US_QUOTE ?></h3><span class="close-quotes">&rdquo;</span>
             </div>
         </div>
     </div><!--end container page-->
     <div class="row randy-quotes">
         <div class="container-page">
             <div class="col-sm-6 quotes">
-                <blockquote>The incredible success of California Fitness & Yoga can be linked to the vision and leadership of Founder and CEO, Randy G. Dobson. Randy brings to his company a passion for life and entertainment that is revolutionizing the way people exercise in Asia.</blockquote>
+                <blockquote><?php echo LANG_ABOUT_US_QUOTE ?></blockquote>
             </div>
         </div>
     </div><!--end row-->
     <div class="container-page">
         <div class="row info">
             <div class="col-sm-6">
-                <p>In 2007, California Fitness & Yoga became the first and largest international fitness company to open in Vietnam. With a mission of “Making Life Better” California Fitness & Yoga is not just another ordinary gym. It’s a dynamic lifestyle center that aims to inspire, entertain and energize the communities it serves.</p>
-                <p>California Fitness & Yoga is a place where fitness, fashion and entertainment merge in a healthy high energy environment. From the music and lighting to the state-of-the art equipment and world class instructors, every detail has been meticulously prepared to provide the greatest possible experience.</p>
+                <p><?php echo LANG_ABOUT_US_PARAGRAPH1 ?></p>
             </div>
             <div class="col-sm-6">
-                <p>The members of CFYC’s  management group have  been key individuals on leadership teams for top fitness industry brands including 24 Hour Fitness, California Fitness, Jackie Chan Sport, UFC Gyms, Crunch Fitness, Yoga Works and Les Mills, just to name a few.</p>
-                <p>With a management board that has over 30 years of combined experience in a dozen different countries, California Fitness & Yoga has one of the most well seasoned and professional leadership teams in the industry; enabling it to consistently deliver its brand promise to Make Life Better.</p>
+                <p><?php echo LANG_ABOUT_US_PARAGRAPH2 ?></p>
             </div>
         </div>
         <div class="row gallery">
