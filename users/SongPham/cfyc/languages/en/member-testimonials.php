1b0fbdca41c70600e52c63927d3f0d6166053f13
aaa <teo@yahoo.com>
2015-08-09 22:23:49 +0700

    update member testimonials page

---
+++ users/SongPham/cfyc/languages/en/member-testimonials.php
@@ -0,0 +1,78 @@
+<?php
+
+define("LANG_MEMBER_TESTIMONIALS", "Member testimonials");
+
+define('LANG_MEMBER_TESTIMONIALS_BANNER_TITLE', 'WE MAKE LIFE BETTER');
+define('LANG_MEMBER_TESTIMONIALS_BANNER_SUB_TITLE', 'OVER 100,000 PEOPLE HAVE TRUSTED US TO LOSE WEIGHT, BUILD MUSCLE AND MAKE THEIR LIFE BETTER VISIT US FOR A FREE MEMBERSHIP TRIAL TODAY');
+define('LANG_MEMBER_TESTIMONIALS_BANNER_TEXT_BUTTON', 'Try a class');
+
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_1', 'story-1.png');
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_1_CAPTION', 'Nataly Pham');
+
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_2', 'story-2.png');
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_2_CAPTION', 'Lâm Phương Bách');
+
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_3', 'story-3.png');
+define('LANG_MEMBER_TESTIMONIALS_CIRCLE_IMAGE_3_CAPTION', 'Ms Võ Huyền Trang');
+
+
+
+define('LANG_MEMBER_TESTIMONIALS_STORY_1_IMAGE', 'success-1.png');
+define('LANG_MEMBER_TESTIMONIALS_STORY_1_BEFORE', '
+        <h3>before</h3>
+        <p>Body fat: 25.3kg</p>
+        <p>Weight: 62.2kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_1_AFTER', '
+        <h3>after</h3>
+        <p>Body fat: 18.5kg</p>
+        <p>Weight: 58.5kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_1_TITLE', 'STORY 1');
+define('LANG_MEMBER_TESTIMONIALS_STORY_1_CONTENT', "
+        <p>Regular fitness keeps me healthy both physically and mentally! I had no idea what kind of world I was getting into when I changed my lifestyle to a more fit and healthy one, but I'm thankful I did. Here are a few ways fitness changed my life for the better.</p>
+        <p>1. It helped me kick bad habits. I would come home, get into workout gear, push myself and grab for water instead of vodka. </p>
+        <p>2. My competition is myself. We live in a society where we constantly compare ourselves to others. What I love about fitness is that your competition is yourself. You always want to do better than you did the last time. </p>
+        <p>3. My best ideas are a result of a good workout. I have had more 'A-HA!' moments while in the gym than anywhere else.</p>
+        <p>4. I crave better foods. Just like how I no longer craved alcohol at the end of every day, when I started exercising more, I began to crave healthier foods. </p>
+        <p>5. I'm more motivated. There's something about reaching small goals that motivates you in other areas in your life as well. The more goals I make through fitness, the more goals I want to make in other areas of my life. Fitness helps improve my career, my relationships and my overall outlook on life. </p>
+        <p>6. I'm capable. The more I work on getting stronger, the more I can do physically. </p>
+        <p>7. I have a can do attitude. Knowing I can reach my fitness goals gives me an I-can-do-anything attitude that is much needed when I feel down. </p>
+        <p>8. I look good.</p>
+    ");
+
+
+
+define('LANG_MEMBER_TESTIMONIALS_STORY_2_IMAGE', 'success-2.png');
+define('LANG_MEMBER_TESTIMONIALS_STORY_2_BEFORE', '
+        <h3>before</h3>
+        <p>Body fat: 20.3kg</p>
+        <p>Weight: 84.2kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_2_AFTER', '
+        <h3>after</h3>
+        <p>Body fat: 6.4kg</p>
+        <p>Weight: 66.9kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_2_TITLE', 'STORY 2');
+define('LANG_MEMBER_TESTIMONIALS_STORY_2_CONTENT', "
+        <p>Bach was only 18 years old when he signed up for BTC9 (Body Transformation Challenge 9) he lost 13.9 kg (16.5%) in 8 weeks. He was attending an American university at the time and returned to Vietnam for vacation where he join BTC. He’s fan of fast food. He thought he’s overweight so he’s not confident in communicating with others and got away from outdoor activities. Bach participated in almost every boot camp that was offered. After the Body Transformation Challenge ended he maintained a healthy diet and disciplined training. He completely re-designed himself by taking the initiative to change his health and his life, got better educational results, became more active…</p>
+    ");
+
+
+
+define('LANG_MEMBER_TESTIMONIALS_STORY_3_IMAGE', 'success-3.png');
+define('LANG_MEMBER_TESTIMONIALS_STORY_3_BEFORE', '
+        <h3>before</h3>
+        <p>Body fat: 20.3kg</p>
+        <p>Weight: 84.2kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_3_AFTER', '
+        <h3>after</h3>
+        <p>Body fat: 6.4kg</p>
+        <p>Weight: 66.9kg</p>
+    ');
+define('LANG_MEMBER_TESTIMONIALS_STORY_3_TITLE', 'STORY 3');
+define('LANG_MEMBER_TESTIMONIALS_STORY_3_CONTENT', "
+        <p>Trang met some difficulties in work and communication because of her appearance (fat body). She looked through some fitness centre in Hanoi and finally found California the best. She followed the guidance of PT. The biggest difficulties was her food appetite. The more workout she did, the hungrier she felt. But PT was strict with Trang about nutrion, so she gradually controlled what to eat for the best result. She lost 9.5kg of fat, gained 3.5kg muscle, increase Inbody point from 66 to 89! She got confidence, will, determination, belief in herself and life!</p>
+    ");
