0cdca0e04e652019fbf972ab4e5b70b471f59ada
aaa <teo@yahoo.com>
2015-08-10 20:33:18 +0700

    update club page

--- users/SongPham/cfyc/pages/club-bien-hoa.php
+++ users/SongPham/cfyc/pages/club-bien-hoa.php
@@ -52,21 +52,12 @@
                             <img src="<?php echo USER_BASE_URL ?>/img/club/slide-1.jpg"  title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
                         </div>
                         <div class="item">
-                            <div class="carousel-caption">
-                                <?php echo LANG_CLUB_BIEN_HOA_CLUB_1_CAPTION_2 ?>
-                            </div>
                             <img src="<?php echo USER_BASE_URL ?>/img/club/slide-1.jpg"  title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
                         </div>
                         <div class="item">
-                            <div class="carousel-caption">
-                                <?php echo LANG_CLUB_BIEN_HOA_CLUB_1_CAPTION_3 ?>
-                            </div>
                             <img src="<?php echo USER_BASE_URL ?>/img/club/slide-1.jpg"  title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
                         </div>
                         <div class="item">
-                            <div class="carousel-caption">
-                                <?php echo LANG_CLUB_BIEN_HOA_CLUB_1_CAPTION_4 ?>
-                            </div>
                             <img src="<?php echo USER_BASE_URL ?>/img/club/slide-1.jpg"  title="Visit us for a free membership trial today" alt="Visit us for a free membership trial today">
                         </div>
                         <div class="carousel-caption">
