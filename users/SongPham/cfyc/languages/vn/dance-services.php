17d85a9ab8f11df9341b62f50c478ccbbba18128
Glen <glencoffey@cfyc.com.vn>
2015-08-10 19:04:04 +0700

    Fixed Language in Services For John

--- users/SongPham/cfyc/languages/vn/dance-services.php
+++ users/SongPham/cfyc/languages/vn/dance-services.php
@@ -1,66 +1,66 @@
 <?php
 
-define("LANG_PAGE_NAME","dance-services");
+define("LANG_DANCE_PAGE_NAME","dance-services");
 
-define("LANG_BANNER_TITLE","Visit us for a free membership trial today");
-define("LANG_BANNER_TEXT_CONTENT","Discover the beauty & excitement of Cali Dance");
-define("LANG_BANNER_TEXT_SMALL","Experience a dance class today");
-define("LANG_BANNER_TEXT_CTA","Try a class");
+define("LANG_DANCE_BANNER_TITLE","Visit us for a free membership trial today");
+define("LANG_DANCE_BANNER_TEXT_CONTENT","Discover the beauty & excitement of Cali Dance");
+define("LANG_DANCE_BANNER_TEXT_SMALL","Experience a dance class today");
+define("LANG_DANCE_BANNER_TEXT_CTA","Try a class");
 
-define("LANG_CALIDANCE","Các lớp nhảy Cali Dance");
-define("LANG_CALIDANCE_CONTENT","Được lựa chọn và chắt lọc từ các chương trình vũ đạo hàng đầu, các lớp nhảy của Cali sẽ thổi bùng năng lượng trong bạn với những vũ điệu và âm nhạc sôi động. Hãy tập luyện với các điệu nhảy yêu thích nhất!<br>
+define("LANG_DANCE_CALIDANCE","Các lớp nhảy Cali Dance");
+define("LANG_DANCE_CALIDANCE_CONTENT","Được lựa chọn và chắt lọc từ các chương trình vũ đạo hàng đầu, các lớp nhảy của Cali sẽ thổi bùng năng lượng trong bạn với những vũ điệu và âm nhạc sôi động. Hãy tập luyện với các điệu nhảy yêu thích nhất!<br>
 Hiện nay, với hơn 10 chương trình nhảy đủ mọi thể loại, việc đốt cháy năng lượng dư thừa sẽ trở nên thú hơn bao giờ hết. Hãy phiêu cùng các vũ điệu La-tinh, giải phóng cơ thể cùng các bước nhảy uyển chuyển đầy quyến rũ của Zumba. Khám phá cơ thể qua những chuyển động tinh tế của cơ thể với lớp múa bụng ấn tượng!<br>
 Tham gia vào lớp nhảy Sh’bam với các điệu nhảy mới được kết hợp với các điệu nhảy hiphop trẻ trung hay tập luyện vùng cơ bụng hiệu quả và tạo dáng ấn tượng cùng lớp Múa cột. Hãy khám phá và theo đuổi những điệu nhảy tốt nhất ngay hôm nay!
 ");
-define("LANG_YOUTUBE","https://www.youtube.com/embed/Ol1nuRXaVcM");
+define("LANG_DANCE_YOUTUBE","https://www.youtube.com/embed/Ol1nuRXaVcM");
 
-define("LANG_BENEFIT","Các lớp học nhảy tại Cali mang đến lợi ích gì cho bạn?");
-define("LANG_BENEFIT_CONTENT","Các điệu nhảy sẽ giúp bạn nhanh nhẹn hơn, bên cạnh đó, sự đa dạng và sôi động của lớp học sẽ giúp bạn gắn kết hơn với các học viên khác, vừa giữ gìn sức khỏe vừa có những khoảnh khắc vui tươi.<br>
+define("LANG_DANCE_BENEFIT","Các lớp học nhảy tại Cali mang đến lợi ích gì cho bạn?");
+define("LANG_DANCE_BENEFIT_CONTENT","Các điệu nhảy sẽ giúp bạn nhanh nhẹn hơn, bên cạnh đó, sự đa dạng và sôi động của lớp học sẽ giúp bạn gắn kết hơn với các học viên khác, vừa giữ gìn sức khỏe vừa có những khoảnh khắc vui tươi.<br>
 Tất cả các hội viên của trung tâm at California Fitness & Yoga đều có quyền tham gia bất kì các lớp học nhảy GroupX, không giới hạn số lượng lớp tham gia
 ");
-define("LANG_DISCOVER","Khám phá các lớp học nhảy tại California Fitness and Yoga");
-define("LANG_DISCOVER_TAB1_TITLE","ZUMBA (M)");
-define("LANG_DISCOVER_TAB1_ID","zumba");
-define("LANG_DISCOVER_TAB1_CONTENT",'
+define("LANG_DANCE_DISCOVER","Khám phá các lớp học nhảy tại California Fitness and Yoga");
+define("LANG_DANCE_DISCOVER_TAB1_TITLE","ZUMBA (M)");
+define("LANG_DANCE_DISCOVER_TAB1_ID","zumba");
+define("LANG_DANCE_DISCOVER_TAB1_CONTENT",'
 <h3>ZUMBA</h3>
 <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
 <p>Những bước nhảy Latin "biến hóa" theo điệu nhạc từ cổ điển đến hiện đại sẽ khiến bạn "phiêu" hết cỡ. Zumba Fitness gồm những chuyển động vừa là bài tập thể dục giúp bạn đốt mỡ vừa là cách giúp bạn trở nên tự tin, gợi cảm hơn.</p>
 ');
-define("LANG_DISCOVER_TAB2_TITLE","BODY JAM (M)");
-define("LANG_DISCOVER_TAB2_ID","bodyjam");
-define("LANG_DISCOVER_TAB2_CONTENT",'
+define("LANG_DANCE_DISCOVER_TAB2_TITLE","BODY JAM (M)");
+define("LANG_DANCE_DISCOVER_TAB2_ID","bodyjam");
+define("LANG_DANCE_DISCOVER_TAB2_CONTENT",'
 <h3>BODY JAM</h3>
 <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
 <p>Những bước nhảy Latin "biến hóa" theo điệu nhạc từ cổ điển đến hiện đại sẽ khiến bạn "phiêu" hết cỡ. Zumba Fitness gồm những chuyển động vừa là bài tập thể dục giúp bạn đốt mỡ vừa là cách giúp bạn trở nên tự tin, gợi cảm hơn.</p>
 ');
-define("LANG_DISCOVER_TAB3_TITLE","SEXY DANCE (M)");
-define("LANG_DISCOVER_TAB3_ID","sexydance");
-define("LANG_DISCOVER_TAB3_CONTENT",'
+define("LANG_DANCE_DISCOVER_TAB3_TITLE","SEXY DANCE (M)");
+define("LANG_DANCE_DISCOVER_TAB3_ID","sexydance");
+define("LANG_DANCE_DISCOVER_TAB3_CONTENT",'
 <h3>SEXY DANCE</h3>
 <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
 <p>Những bước nhảy Latin "biến hóa" theo điệu nhạc từ cổ điển đến hiện đại sẽ khiến bạn "phiêu" hết cỡ. Zumba Fitness gồm những chuyển động vừa là bài tập thể dục giúp bạn đốt mỡ vừa là cách giúp bạn trở nên tự tin, gợi cảm hơn.</p>
 ');
-define("LANG_DISCOVER_TAB4_TITLE","POLE DANCE (M)");
-define("LANG_DISCOVER_TAB4_ID","poledance");
-define("LANG_DISCOVER_TAB4_CONTENT",'
+define("LANG_DANCE_DISCOVER_TAB4_TITLE","POLE DANCE (M)");
+define("LANG_DANCE_DISCOVER_TAB4_ID","poledance");
+define("LANG_DANCE_DISCOVER_TAB4_CONTENT",'
 <h3>POLE DANCE</h3>
 <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
 <p>Những bước nhảy Latin "biến hóa" theo điệu nhạc từ cổ điển đến hiện đại sẽ khiến bạn "phiêu" hết cỡ. Zumba Fitness gồm những chuyển động vừa là bài tập thể dục giúp bạn đốt mỡ vừa là cách giúp bạn trở nên tự tin, gợi cảm hơn.</p>
 ');
-define("LANG_DISCOVER_TAB5_TITLE","SALSA & LATIN DANCE (M)");
-define("LANG_DISCOVER_TAB5_ID","salsa");
-define("LANG_DISCOVER_TAB5_CONTENT",'
+define("LANG_DANCE_DISCOVER_TAB5_TITLE","SALSA & LATIN DANCE (M)");
+define("LANG_DANCE_DISCOVER_TAB5_ID","salsa");
+define("LANG_DANCE_DISCOVER_TAB5_CONTENT",'
 <h3>SALSA & LATIN DANCE</h3>
 <p><strong>MULTI LEVEL: SUITABLE FOR BEGINNER TO ADVANCED.</strong></p>
 <p>Những bước nhảy Latin "biến hóa" theo điệu nhạc từ cổ điển đến hiện đại sẽ khiến bạn "phiêu" hết cỡ. Zumba Fitness gồm những chuyển động vừa là bài tập thể dục giúp bạn đốt mỡ vừa là cách giúp bạn trở nên tự tin, gợi cảm hơn.</p>
 ');
-define("LANG_ENVIRONMENT","THE PERFECT ENVIRONMENT FOR YOU TO SUCCEED");
-define("LANG_TRAINER_TITLE","Meet our featured dance teachers");
-define("LANG_TRAINER_SUB","You have the potential, we can help you reach it.");
+define("LANG_DANCE_ENVIRONMENT","THE PERFECT ENVIRONMENT FOR YOU TO SUCCEED");
+define("LANG_DANCE_TRAINER_TITLE","Meet our featured dance teachers");
+define("LANG_DANCE_TRAINER_SUB","You have the potential, we can help you reach it.");
 
-define("LANG_TRAINER1_TITLE","DJ");
-define("LANG_TRAINER1_ID","trainer1");
-define("LANG_TRAINER1_CONTENT",'
+define("LANG_DANCE_TRAINER1_TITLE","DJ");
+define("LANG_DANCE_TRAINER1_ID","trainer1");
+define("LANG_DANCE_TRAINER1_CONTENT",'
 <h3>DJ</h3>
 <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
 <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
@@ -69,9 +69,9 @@ define("LANG_TRAINER1_CONTENT",'
 <p></p>
 <p>DJ phát hiện ra niềm đam mê nhảy múa khi đến tuổi 15. Anh đã trải qua hơn 7 năm kinh nghiệm luyện tập vũ đạo và biểu diễn với tư cách là một vũ công hip-hop. DJ luôn say sưa luyện tập, tự thách thức bản thân và khám phá khả năng của mình trong thể dục và thể thao. Lý tưởng sống của anh là tiếp tục học các phong cách nhảy múa khác và luôn phấn đấu trở thành một vũ công xuất sắc.</p>
 ');
-define("LANG_TRAINER2_TITLE","TYLER LY");
-define("LANG_TRAINER2_ID","trainer2");
-define("LANG_TRAINER2_CONTENT",'
+define("LANG_DANCE_TRAINER2_TITLE","TYLER LY");
+define("LANG_DANCE_TRAINER2_ID","trainer2");
+define("LANG_DANCE_TRAINER2_CONTENT",'
 <h3>TYLER LY</h3>
 <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
 <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
@@ -80,9 +80,9 @@ define("LANG_TRAINER2_CONTENT",'
 <p></p>
 <p>DJ phát hiện ra niềm đam mê nhảy múa khi đến tuổi 15. Anh đã trải qua hơn 7 năm kinh nghiệm luyện tập vũ đạo và biểu diễn với tư cách là một vũ công hip-hop. DJ luôn say sưa luyện tập, tự thách thức bản thân và khám phá khả năng của mình trong thể dục và thể thao. Lý tưởng sống của anh là tiếp tục học các phong cách nhảy múa khác và luôn phấn đấu trở thành một vũ công xuất sắc.</p>
 ');
-define("LANG_TRAINER3_TITLE","GENESIS");
-define("LANG_TRAINER3_ID","trainer3");
-define("LANG_TRAINER3_CONTENT",'
+define("LANG_DANCE_TRAINER3_TITLE","GENESIS");
+define("LANG_DANCE_TRAINER3_ID","trainer3");
+define("LANG_DANCE_TRAINER3_CONTENT",'
 <h3>GENESIS</h3>
 <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
 <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
@@ -91,9 +91,9 @@ define("LANG_TRAINER3_CONTENT",'
 <p></p>
 <p>DJ phát hiện ra niềm đam mê nhảy múa khi đến tuổi 15. Anh đã trải qua hơn 7 năm kinh nghiệm luyện tập vũ đạo và biểu diễn với tư cách là một vũ công hip-hop. DJ luôn say sưa luyện tập, tự thách thức bản thân và khám phá khả năng của mình trong thể dục và thể thao. Lý tưởng sống của anh là tiếp tục học các phong cách nhảy múa khác và luôn phấn đấu trở thành một vũ công xuất sắc.</p>
 ');
-define("LANG_TRAINER4_TITLE","KATSU");
-define("LANG_TRAINER4_ID","trainer4");
-define("LANG_TRAINER4_CONTENT",'
+define("LANG_DANCE_TRAINER4_TITLE","KATSU");
+define("LANG_DANCE_TRAINER4_ID","trainer4");
+define("LANG_DANCE_TRAINER4_CONTENT",'
 <h3>KATSU</h3>
 <h4>ĐIỀU PHỐI VIÊN GROUP X</h4>
 <p>"Tôi rất thích được chia sẻ niềm đam mê nhảy múa. Khi âm nhạc khiến cho cơ thể bạn không ngừng lắc lư và chân nhún nhảy theo nhịp – tôi gọi đó là Vũ Điệu Cảm Xúc. Tôi luôn được truyền cảm hứng khi ngắm nhìn các học viên của mình vui vẻ, hạnh phúc, chăm chỉ luyện tập và rồi trở nên mạnh khỏe hơn, yêu đời hơn."</p>
