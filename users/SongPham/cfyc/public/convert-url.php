905080c9542365a3047e5fa9ea5cc1a04ffc2616
Yen <a@yahoo.com>
2015-08-24 12:42:22 +0700

    language changed

--- users/SongPham/cfyc/public/convert-url.php
+++ users/SongPham/cfyc/public/convert-url.php
@@ -1,20 +1,13 @@
 <?php
 $coreUrl['club-hcm'] = 'cau-lac-bo-tai-ho-chi-minh-pa3lo1lg2.html';
 $coreUrl['club-hcm/?lang=en'] = 'clubs-in-ho-chi-minh-pa3lo1lg1.html';
-$coreUrl['club-ha-noi'] = 'cau-lac-bo-tai-ha-noi-pa3lo2lg2.html';
-$coreUrl['club-ha-noi/?lang=en'] = 'clubs-in-ha-noi-pa3lo2lg1.html';
-$GLOBALS['urlRedirect'] = array(
-    'club-hcm' => $coreUrl['club-hcm'],
-    'club-hcm/?lang=en' => $coreUrl['club-hcm/?lang=en'],
-    'club-ha-noi' => $coreUrl['club-ha-noi'],
-    'club-ha-noi/?lang=en' => $coreUrl['club-ha-noi/?lang=en']
-);
-$GLOBALS['urlRedirectTranslated'] = array(
-    'club-hcm-vn' => $coreUrl['club-hcm'],
-    'club-hcm-en' => $coreUrl['club-hcm/?lang=en'],
-    'club-ha-noi-vn' => $coreUrl['club-ha-noi'],
-    'club-ha-noi-en' => $coreUrl['club-ha-noi/?lang=en']
-);
+/* url redirect section */
+$GLOBALS['urlRedirect']['club-hcm'] = $coreUrl['club-hcm'];
+$GLOBALS['urlRedirect']['club-hcm/?lang=en'] = $coreUrl['club-hcm/?lang=en'];
+/* url redirect translated */
+$GLOBALS['urlRedirectTranslated']['club-hcm-vn'] = $coreUrl['club-hcm'];
+$GLOBALS['urlRedirectTranslated']['club-hcm-en'] = $coreUrl['club-hcm/?lang=en'];
+include('convert-url-1.php');
 function createUrl ($key) {
     return $GLOBALS['urlRedirect'][$key];
 };
