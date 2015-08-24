d83c8fbfb356e353eab67a41a76ea44bd02f1f4f
Glen <glencoffey@cfyc.com.vn>
2015-08-24 16:18:17 +0700

    thank you page added

---
+++ users/SongPham/cfyc/languages/en/cam-on.php
@@ -0,0 +1,5 @@
+<?php
+define('LANG_THANK_YOU_PAGE_TITLE','Thank You!');
+define('LANG_THANK_YOU_PAGE_SUBTITLE','Thank you for your interest in California Fitness & Yoga’s unique, fun and exciting life enhancement fitness programs. We will contact you within 24 hours');
+define('LANG_THANK_YOU_PAGE_VIDEO_DESC','See Our exciting new fitness program - PTX. Coming to a club near you');
+define('LANG_THANK_YOU_PAGE_SECOND_TITLE','California Fitness &amp; Yoga is celebrating 22 clubs in 8 years. Find one near you!');
